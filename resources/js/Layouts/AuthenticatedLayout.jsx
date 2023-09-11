1dc515e4e80f6bbcbbbde3fa314bd9808948cd9e
shigaresso <70837100+shigaresso@users.noreply.github.com>
2023-09-11 15:39:20 +0900

    レイアウトのナビゲーションリンクに chirps/index へ遷移できる UI の追加。

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -24,6 +24,9 @@ export default function Authenticated({ user, header, children }) {
                                 <NavLink href={route('dashboard')} active={route().current('dashboard')}>
                                     Dashboard
                                 </NavLink>
+                                <NavLink href={route('chirps.index')} active={route().current('chirps.index')}>
+                                    Chirps
+                                </NavLink>
                             </div>
                         </div>
 
