596e08eb1b5027b6d8d4502298410667c69132d1
shigaresso <70837100+shigaresso@users.noreply.github.com>
2023-09-11 15:42:06 +0900

    モバイル画面でも chirps/index に移動するためのナビゲーションリンクの追加

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -98,6 +98,9 @@ export default function Authenticated({ user, header, children }) {
                         <ResponsiveNavLink href={route('dashboard')} active={route().current('dashboard')}>
                             Dashboard
                         </ResponsiveNavLink>
+                        <ResponsiveNavLink href={route('chirps.index')} active={route().current('chirps.index')}>
+                            Chirps
+                        </ResponsiveNavLink>
                     </div>
 
                     <div className="pt-4 pb-1 border-t border-gray-200">
